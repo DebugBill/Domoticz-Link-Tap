3183b6dc1c2ac9e6f3a0512a547762aa6fa16406
DebugBill <debugbill@thauvin.org>
2021-06-08 00:06:19 +0200

    Parameters page OK

--- plugin.py
+++ plugin.py
@@ -6,10 +6,12 @@
 # Author: DebugBill
 #
 """
-<plugin key="linktap" name="Link-Tap plugin" author="DebugBill" version="0.1.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://link-tap.com/">
+<plugin key="linktap" name="Link-Tap Watering System plugin" author="DebugBill" version="0.1.0" wikilink="http://www.domoticz.com/wiki/plugins/plugin.html" externallink="https://github.com/DebugBill/Link-Tap">
     <description>
         <h2>Link-Tap watering system</h2><br/>
-        Overview...
+        This plugin will allow Domoticz to read data from the Link-Tap cloud API.
+        API key is required
+        More info on Link-Tap hardware can be found at https://link-tap.com 
         <h3>Features</h3>
         <ul style="list-style-type:square">
             <li>Feature one...</li>
@@ -23,16 +25,29 @@
         Configuration options...
     </description>
     <params>
-        <param field="URL" label="Link-Tap API URL" width="200px" required="true" default="https://www.link-tap.com/"/>
-        <param field="User" label="User" width="30px" required="true"/>
-        <param field="Key" label="Key" width="150px" required="true"/>
+        <param field="Mode1" label="Link-Tap API URL" width="300px" required="true" default="https://www.link-tap.com/"/>
+        <param field="Mode2" label="User" width="300px" required="true"/>
+        <param field="Mode3" label="Key" width="300px" required="true"/>
+        <param field="Mode4" label="Polling internval" width="100px">
+            <options>
+                <option label="5mn" value="10" defautl="true"/>
+                <option label="10mn" value="20"/>
+                <option label="15mn" value="30"/>
+                <option label="30mn" value="60" />
+            </options>
+        </param>
+        <param field="Mode6" label="Debug" width="75px">
+            <options>
+                <option label="True" value="Debug"/>
+                <option label="False" value="Normal"  default="true" />
+            </options>
+        </param>
     </params>
 </plugin>
 """
 import Domoticz
 import json
 import requests
-from Domoticz import Device
 
 class BasePlugin:
     enabled = False
@@ -41,7 +56,12 @@ class BasePlugin:
         return
 
     def onStart(self):
+        if Parameters["Mode6"] == "Debug":
+            Domoticz.Debugging(1)
+        else:
+            Domoticz.Debugging(0)
         Domoticz.Log("onStart called")
+	DumpAllToLog()
 
     def onStop(self):
         Domoticz.Log("onStop called")
@@ -113,3 +133,52 @@ def DumpConfigToLog():
         Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
         Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
     return
+
+
+def DumpDevicesToLog():
+    # Show devices
+    Domoticz.Debug("Device count.........: {}".format(len(Devices)))
+    for x in Devices:
+        Domoticz.Debug("Device...............: {} - {}".format(x, Devices[x]))
+        Domoticz.Debug("Device Idx...........: {}".format(Devices[x].ID))
+        Domoticz.Debug(
+            "Device Type..........: {} / {}".format(Devices[x].Type, Devices[x].SubType)
+        )
+        Domoticz.Debug("Device Name..........: '{}'".format(Devices[x].Name))
+        Domoticz.Debug("Device nValue........: {}".format(Devices[x].nValue))
+        Domoticz.Debug("Device sValue........: '{}'".format(Devices[x].sValue))
+        Domoticz.Debug("Device Options.......: '{}'".format(Devices[x].Options))
+        Domoticz.Debug("Device Used..........: {}".format(Devices[x].Used))
+        Domoticz.Debug("Device ID............: '{}'".format(Devices[x].DeviceID))
+        Domoticz.Debug("Device LastLevel.....: {}".format(Devices[x].LastLevel))
+        Domoticz.Debug("Device Image.........: {}".format(Devices[x].Image))
+
+
+def DumpImagesToLog():
+    # Show images
+    Domoticz.Debug("Image count..........: {}".format((len(Images))))
+    for x in Images:
+        Domoticz.Debug("Image '{}'...: '{}'".format(x, Images[x]))
+
+
+def DumpParametersToLog():
+    # Show parameters
+    Domoticz.Debug("Parameters count.....: {}".format(len(Parameters)))
+    for x in Parameters:
+        if Parameters[x] != "":
+            Domoticz.Debug("Parameter '{}'...: '{}'".format(x, Parameters[x]))
+
+
+def DumpSettingsToLog():
+    # Show settings
+    Domoticz.Debug("Settings count.......: {}".format(len(Settings)))
+    for x in Settings:
+        Domoticz.Debug("Setting '{}'...: '{}'".format(x, Settings[x]))
+
+
+def DumpAllToLog():
+    DumpDevicesToLog()
+    DumpImagesToLog()
+    DumpParametersToLog()
+    DumpSettingsToLog()
+
